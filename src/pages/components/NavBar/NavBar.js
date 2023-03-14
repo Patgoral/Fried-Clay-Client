9bd6b89b34292b9f4122d1ac4ffc46be138a38fe
Patrick Goral <patgoral@gmail.com>
2023-03-14 14:34:42 -0400

    deploytest

--- src/pages/components/NavBar/NavBar.js
+++ src/pages/components/NavBar/NavBar.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import * as userService from '../../../utilities/users-services'
 import './NavBar.css'
 import logo from '../../../images/logo.png'
+import headlogo from '../../../images/headlogo.png'
 
 export default function NavBar({ user, setUser }) {
 	function handleLogOut() {
@@ -12,13 +13,25 @@ export default function NavBar({ user, setUser }) {
 	return (
 		<div className="top-nav-container">
 			<div className="top-nav">
-				<Link to="/"> <img className="navlogo" src={logo} alt={'logo'}/></Link>
-				<nav className='nav'>
-                <Link className='link' to="/update">Update Submission</Link> 
-				<span className="divider"></span> &nbsp;
-                <Link className='link' to="" onClick={handleLogOut}>Log Out</Link> 
-
-                </nav>
+				<Link to="/">
+					{' '}
+					<img className="navlogo" src={logo} alt={'logo'} />
+				</Link>
+				<nav className="nav top-nav">
+					{' '}
+					<Link to="/">
+						{' '}
+						<img className="headlogo" src={headlogo} alt={'logo'} />
+					</Link>
+					<span className="divider"></span> &nbsp;
+					<Link className="link" id="update" to="/update">
+						Update Submission
+					</Link>
+					<span className="divider"></span> &nbsp;
+					<Link className="link" id="logout" to="" onClick={handleLogOut}>
+						Log Out
+					</Link>
+				</nav>
 			</div>
 		</div>
 	)
