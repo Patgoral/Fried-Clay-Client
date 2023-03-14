51f319286c9be4ea4bf04fbd7bb4514d0e9e17fd
Patrick Goral <patgoral@gmail.com>
2023-03-14 13:01:50 -0400

    dependencies

--- config/database.js
+++
@@ -1,9 +0,0 @@
-const django = require('django')
-
-django.connect(process.env.DATABASE_URL)
-
-const db = django.connect
-
-db.on('connected', function() {
-    console.log(`Connected to ${db.name} at ${db.host}:${db.port}`)
-})
--- src/utilities/attendees-api.js
+++ src/utilities/attendees-api.js
@@ -1,6 +1,6 @@
 import {sendRequest, sendRequestToDelete, sendFormRequest} from './users-api'
 
-const BASE_URL = 'https://trackr-server.onrender.com/api/attendees'
+const BASE_URL = 'http://localhost:3000/api/attendees'
 
 export function showAttendees() {
     return sendRequest(BASE_URL)}
