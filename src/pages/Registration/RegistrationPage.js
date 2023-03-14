32aa3d8684bf028b1a91edbfbdd6d5d6c5e04fc5
Patrick Goral <patgoral@gmail.com>
2023-03-14 10:55:57 -0400

    edits

--- src/pages/Registration/RegistrationPage.js
+++ src/pages/Registration/RegistrationPage.js
@@ -9,24 +9,41 @@ export default function RegistrationPage() {
 	const [name, setName] = useState('')
 	const [date, setDate] = useState(new Date())
 	const [image, setImage] = useState(undefined)
-    const [gpx, setGpx] = useState(undefined)
+	const [gpx, setGpx] = useState(undefined)
 
 	async function handleAddAttendee(event) {
 		event.preventDefault()
-        const formData = new FormData()
+		const formData = new FormData()
 
-        // append the attendee name and date to the formData object
-        formData.append('attendee[name]', name)
-        formData.append('attendee[date]', date)
-    
-        // append the image file to the formData object
-        formData.append('image', image)
-        formData.append('gpx', gpx)
-        console.log('formData:', formData)
-    
-        await attendeesAPI.addAttendee(formData)
-        navigate('/')
+        if (!name) {
+            document.getElementById('message-container').innerHTML = 'Name Field Is Required';
+            return;
+          }
+
+          if (!image) {
+            document.getElementById('message-container').innerHTML = 'Image Upload Is Required';
+            return;
+          }
+
+		// append the attendee name and date to the formData object
+		formData.append('attendee[name]', name)
+		formData.append('attendee[date]', date)
+
+		// append the image file to the formData object
+		formData.append('image', image)
+        
+        const gpxFile = gpx && gpx.name.endsWith('.gpx')
+        ? gpx
+        : undefined;
+      if (!gpxFile) {
+        document.getElementById('message-container').innerHTML = 'Invalid GPX file type';
+        return;
       }
+      formData.append('gpx', gpxFile);		
+
+		await attendeesAPI.addAttendee(formData)
+		navigate('/')
+	}
 
 	function handleInputChange(event) {
 		setName(event.target.value)
@@ -37,7 +54,11 @@ export default function RegistrationPage() {
 			<div className="register-page">
 				<div className="register-form-container">
 					<div className="register-header">Submit Your Time</div>
-					<form className="register-form" onSubmit={handleAddAttendee} encType="multipart/form">
+					<form
+						className="register-form"
+						onSubmit={handleAddAttendee}
+						encType="multipart/form"
+					>
 						<div>
 							<input
 								placeholder="Name"
@@ -51,13 +72,15 @@ export default function RegistrationPage() {
 								onChange={setDate}
 								value={date}
 							/>
+							<p>Upload Image</p>
 							<input
 								type="file"
-                                
 								name="image"
 								onChange={(e) => setImage(e.target.files[0])}
 							/>
-                            	<input
+							<p>Upload GPX File</p>
+
+							<input
 								type="file"
 								name="gpx"
 								onChange={(e) => setGpx(e.target.files[0])}
@@ -67,6 +90,7 @@ export default function RegistrationPage() {
 							Submit
 						</button>
 					</form>
+                    <h3 id="message-container">&nbsp;</h3>
 				</div>
 			</div>
 		</div>
--- src/pages/components/AttendeeDetail/AttendeeDetail.js
+++ src/pages/components/AttendeeDetail/AttendeeDetail.js
@@ -1,50 +1,73 @@
-import "./AttendeeDetail.css"
-import { useParams, useLocation } from "react-router-dom";
+import './AttendeeDetail.css'
+import { useParams, useLocation } from 'react-router-dom'
 import { indexEachAttendee } from '../../../utilities/attendees-api'
 import { useState, useEffect } from 'react'
-import { elapsedTime } from "../../utils/dateFormatter";
-import  MapComponent  from "../MapComponent/MapComponent"
+import { elapsedTime } from '../../utils/dateFormatter'
+import MapComponent from '../MapComponent/MapComponent'
 
 export default function AttendeeDetailPage() {
-  const [attendee, setAttendee] = useState({ name: '', date: '', image: '', gpx: ''})
-  const [isDataReady, setIsDataReady] = useState(false);
-  const { id } = useParams();
-  const location = useLocation()
-  const { position } = location.state
- 
-    useEffect(() => {
-      let active = true
-      async function getSingleAttendee() {
-        const singleAttendee = await indexEachAttendee(id)
-      setAttendee(singleAttendee.attendees)
+	const [attendee, setAttendee] = useState({
+		name: '',
+		date: '',
+		image: '',
+		gpx: '',
+	})
+	const [isDataReady, setIsDataReady] = useState(false)
 
-      setIsDataReady(true); // set the flag to true when the data is ready
-     
+	const { id } = useParams()
+	const location = useLocation()
+	const { position } = location.state
 
-      }
-      getSingleAttendee()
+	useEffect(() => {
+		let active = true
+		async function getSingleAttendee() {
+			const singleAttendee = await indexEachAttendee(id)
+			setAttendee(singleAttendee.attendees)
+
+			setIsDataReady(true) // set the flag to true when the data is ready
+		}
+		getSingleAttendee()
+
+		return () => {
+			active = false
+		}
+	}, [id])
+
+	return (
+		<>
       
-      return () => {
-        active = false
-      }
-    },[id])
- 
+        <div className='attendee-detail-container'>
+        {!isDataReady && (
+          <div className="lds-roller">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        )}
+          {isDataReady && (
+			  <div className="attendee-detail">
+       
+            <h2>Title: {attendee.name} </h2>
+            <h2>Finishing Time: {elapsedTime(attendee.date)} </h2>
+            <h2>Position: {position ?? 'Check Back Later'}</h2>
 
-
-  return (
-    <>
-    <div className="attendee-detail">
-     <h2>Title: {attendee.name} </h2>
-     <h2>Finishing Time: {elapsedTime(attendee.date)} </h2>
-     <h2>Position: {position ?? 'Check Back Later'}</h2>
-   
-    <div className="img">
-    <img width='400px' src={attendee.image} />
-    </div>
-    <div  className="map">
-      <MapComponent gpx={attendee.gpx}/>
+            <div className="img">
+              <img width='400px' src={attendee.image} />
+            </div>
+            <div className="map">
+              <MapComponent gpx={attendee.gpx}/>
+            </div>
+          
+       
       </div>
+      )}
       </div>
-   </>
+     
+    </>
   );
 }
