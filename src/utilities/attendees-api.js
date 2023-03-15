e8e147badf5cbc677080fcf0704836ac89dcfc7b
Patrick Goral <patgoral@gmail.com>
2023-03-14 21:13:48 -0400

    fix

--- src/utilities/attendees-api.js
+++ src/utilities/attendees-api.js
@@ -1,6 +1,6 @@
 import {sendRequest, sendRequestToDelete, sendFormRequest} from './users-api'
 
-const BASE_URL = 'https://trackr-client.onrender.com/api/attendees'
+const BASE_URL = 'https://trackr-server.onrender.com/api/attendees'
 
 export function showAttendees() {
     return sendRequest(BASE_URL)}
