42055f92d569315229bbaa47e5488e4a347b8a96
Patrick Goral <patgoral@gmail.com>
2023-03-20 12:41:58 -0400

    catchall

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -28,6 +28,8 @@ function App() {
 						<Route path="/update" element={<UpdatePage />} />
 						<Route path="/access" element={<AccessPage />} />
             <Route path="/attendees/:id" element={<AttendeeDetail />} />
+			<Route path="*" element={<EventPage />} />
+   
 			
 					</Routes>
 				</>
