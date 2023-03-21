7b2a9d46028c33e3ee865cb9d9d1cfea12b31d5f
Patrick Goral <patgoral@gmail.com>
2023-03-21 10:02:11 -0400

    devcatchall

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -30,6 +30,8 @@ function App() {
 						/>
 						<Route path="/update" element={<UpdatePage />} />
             <Route path="/attendees/:id" element={<AttendeeDetail />} />
+			<Route path="*" element={<EventPage />} />
+
 					</Routes>
 				</>
 			) : (
