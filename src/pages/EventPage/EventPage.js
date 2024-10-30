29d2a46d561e1640d3ac685e597c3aee55f60e05
patGoral <patgoral@gmail.com>
2024-10-30 11:31:08 -0400

    test

--- src/pages/EventPage/EventPage.js
+++ src/pages/EventPage/EventPage.js
@@ -81,7 +81,7 @@ export default function EventPage() {
 	}
 	return (
 		<div className="event-page">
-			<div className="event-page-container-top">
+			{/* <div className="event-page-container-top">
 				<div className="link" to="/">
 					<img width="300px" alt="logo" src={logo} />
 				</div>
@@ -111,29 +111,9 @@ export default function EventPage() {
 						<p className="dead">Submissions Closed</p>
 					)}
 				</div>
-			</div>
+			</div> */}
 			<div className="event-page-list-container">
-				<div className="attendees-container">
-					<div className="attendees-header">Leaderboard</div>
-					<p>Click a name to view details</p>
-
-					<div className="message-container">{messagecontainer}</div>
-
-					{!isPageLoaded && (
-						<div className="lds-roller">
-							<div></div>
-							<div></div>
-							<div></div>
-							<div></div>
-							<div></div>
-							<div></div>
-							<div></div>
-							<div></div>
-						</div>
-					)}
-
-					<div className="list-container-overflow">{attendeeList}</div>
-				</div>
+			<iframe src="https://www.eventbrite.com/e/fried-clay-200k-2025-tickets-1046921529477" width="100%" height="400" />
 			</div>
 		</div>
 	)
