40cda18cb2085f8350a23fcab23413096986910b
patGoral <patgoral@gmail.com>
2025-02-10 15:21:48 -0500

    done?

--- src/pages/Registration/RegistrationPage.js
+++ src/pages/Registration/RegistrationPage.js
@@ -142,7 +142,7 @@ export default function RegistrationPage() {
 							<DateTimePicker
 								disableClock={true}
 								className="date-time"
-								minDate={new Date('03/22/2025 4:30 PM EST')}
+								minDate={new Date('03/22/2025 2:00 PM EST')}
 								maxDate={new Date('03/26/2025 9:00 PM EST')}
 								onChange={setDate}
 								value={date}
