b1a86fa8405e3e72c6141c13e4c811eed0c7d593
patGoral <patgoral@gmail.com>
2025-02-11 09:28:43 -0500

    results

--- src/pages/PastResults/PastResults.js
+++ src/pages/PastResults/PastResults.js
@@ -35,6 +35,18 @@ export default function PastResults() {
 						<Link className="link" to="/2023">
 						 2023 Results
 						</Link>
+
+						<a className="link" href="https://docs.google.com/spreadsheets/d/1oheaS0YIwqe8kJJMTjZofXlFKUJp877x/edit?usp=sharing&ouid=113958247060583909460&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">
+							2022 Results
+						</a>
+
+						<a className="link" href="https://docs.google.com/spreadsheets/d/1qllYZgAVuyfyhEMk4Bc1y8SBInb_F_sX/edit?usp=sharing&ouid=113958247060583909460&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">
+							2021 Results
+						</a>
+
+						<a className="link" href="https://docs.google.com/spreadsheets/d/1snno4L1Cueq78q9c3oLrKJYHn254H7RR/edit?usp=sharing&ouid=113958247060583909460&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">
+							2019 Results
+						</a>
 	
 						
 					</div>
