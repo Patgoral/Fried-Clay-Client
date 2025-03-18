b0374f31767b3fa29f101b82a8e82e4fd1a31c7b
patGoral <patgoral@gmail.com>
2025-03-18 10:11:51 -0400

    gender

--- src/pages/components/AttendeeDetail/AttendeeDetail.js
+++ src/pages/components/AttendeeDetail/AttendeeDetail.js
@@ -65,7 +65,8 @@ export default function AttendeeDetailPage() {
             <div className='attendee-info'>
 						<h2>Name: {attendee.name} </h2>
 						{attendee.gender && <h2>Gender: {attendee.gender}</h2>}
-						<h2>Finishing Time: {elapsedTime(attendee.date)} </h2>
+						<h2>Gender Position: {attendee.genderPosition} </h2>
+ 						<h2>Finishing Time: {elapsedTime(attendee.date)} </h2>
 						<h2>Overall Position: {position ?? 'Check Back Later'}</h2>
             </div>
 						<div className="img">
