df9ad3c95e5c795f8cb8dc46f1fe36931cac6849
patGoral <patgoral@gmail.com>
2025-03-18 10:09:53 -0400

    check

--- src/pages/components/AttendeeCard/AttendeeCard.js
+++ src/pages/components/AttendeeCard/AttendeeCard.js
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 import { dateFormatter } from "../../utils/dateFormatter";
 import './AttendeeCard.css'
 
-export default function AttendeeCard({ attendee, index, genderPosition }) {
+export default function AttendeeCard({ attendee, index }) {
    
 
   return (
     
     <div className='attendee-card' >
-      {index+1}&nbsp;&nbsp;&nbsp;{genderPosition}
+      {index+1}&nbsp;&nbsp;&nbsp;{attendee.genderPosition}
       <Link className="link" state={{ position: index+1 }} to={`/attendees/${attendee._id}`} style={{ color: "#dbafea"}}>
       {attendee.name}, {dateFormatter(attendee.date)}
       </Link>
