ed41fe2fb808f19bbb538cb3f9ee6b45d0ebc953
patGoral <patgoral@gmail.com>
2025-03-18 11:42:42 -0400

    yellow

--- src/pages/components/AttendeeCard/AttendeeCard.js
+++ src/pages/components/AttendeeCard/AttendeeCard.js
@@ -4,28 +4,29 @@ import './AttendeeCard.css'
 
 export default function AttendeeCard({ attendee, index }) {
 
-  const getGenderSuffix = (genderPosition) => {
-    if (genderPosition === "Male 1") return "1st Male";
-    if (genderPosition === "Female 1") return "1st Female";
-    if (genderPosition === "Non-Binary 1") return "1st Non-Binary";
-    return "";
-  };
-  
-  const genderSuffix = attendee?.genderPosition ? getGenderSuffix(attendee.genderPosition) : "";
-  
+const getGenderSuffix = (genderPosition) => {
+  if (genderPosition === "Male 1") return "1st Male";
+  if (genderPosition === "Female 1") return "1st Female";
+  if (genderPosition === "Non-Binary 1") return "1st Non-Binary";
+  return "";
+};
 
-  return (
-    <div className='attendee-card'>
-      {index + 1}&nbsp;&nbsp;&nbsp;
-      <Link 
-        className="link" 
-        state={{ position: index + 1, genderPosition: attendee.genderPosition }}
-        to={`/attendees/${attendee._id}`} 
-        style={{ color: "#dbafea" }}
-      >
-        {attendee.name}, {dateFormatter(attendee.date)} 
-        {genderSuffix ? <span style={{ color: "yellow" }}> - {genderSuffix}</span> : ""}
-      </Link>
-    </div>
-  );
-  
+const genderSuffix = attendee?.genderPosition ? getGenderSuffix(attendee.genderPosition) : "";
+
+
+return (
+  <div className='attendee-card'>
+    {index + 1}&nbsp;&nbsp;&nbsp;
+    <Link 
+      className="link" 
+      state={{ position: index + 1, genderPosition: attendee.genderPosition }}
+      to={`/attendees/${attendee._id}`} 
+      style={{ color: "#dbafea" }}
+    >
+      {attendee.name}, {dateFormatter(attendee.date)} 
+      {genderSuffix ? <span style={{ color: "yellow" }}> - {genderSuffix}</span> : ""}
+    </Link>
+  </div>
+);
+
+}
