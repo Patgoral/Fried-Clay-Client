e703a4bfac351b452fb1b5d4dcf068a48d47cdea
patGoral <patgoral@gmail.com>
2025-03-25 09:27:38 -0400

    fixing tie

--- src/pages/Results2025/Results2025.js
+++ src/pages/Results2025/Results2025.js
@@ -38,18 +38,22 @@ export default function EventPage() {
 				"Non-Binary": 0
 			}
 
+			let tieGroupCount = 1  // To track how many people are in the current tie group
+
 			attendees.attendees.forEach((attendee, index) => {
 				// Assign position for the first attendee or non-tie
 				if (index === 0 || attendee.date !== attendees.attendees[index - 1].date) {
 					attendee.position = currentPos
+					tieGroupCount = 1  // Reset the tie group count for new position
 				} else {
 					// Use the same position for ties
 					attendee.position = attendees.attendees[index - 1].position
+					tieGroupCount++
 				}
 			
-				// Increment position only when moving past a tie group
+				// Increment currentPos only after finishing a tie group
 				if (index === attendees.attendees.length - 1 || attendee.date !== attendees.attendees[index + 1].date) {
-					currentPos = index + 2  // Move to the next rank based on index
+					currentPos += tieGroupCount  // Move to the next rank based on the number of tied attendees
 				}
 			
 				// Gender position logic
