483f54d7c6f06a9acfff33d3401b6d026321dc52
patGoral <patgoral@gmail.com>
2026-03-05 12:04:34 -0500

    test results

--- src/pages/Results2026/Results2026.js
+++ src/pages/Results2026/Results2026.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 import logo from '../../images/FriedClay200k26.png'
 
 const endDate = new Date('03/30/2026')
-const startDate = new Date('03/21/2026')
+const startDate = new Date('03/01/2026')
 startDate.setHours(8, 0, 0, 0)
 
 export default function EventPage() {
