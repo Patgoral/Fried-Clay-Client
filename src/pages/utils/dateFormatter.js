3a13116d2be64257a0f676c45a27652d5c402bb7
patGoral <patgoral@gmail.com>
2026-03-06 10:53:20 -0500

    testing time

--- src/pages/utils/dateFormatter.js
+++ src/pages/utils/dateFormatter.js
@@ -19,7 +19,9 @@ export const elapsedTime = (formattedDate) => {
 	const year = date.getFullYear();
 	
 	const targetDate = new Date(
-	  year === 2025 ? "3/22/2025 8:00 AM" :
+	  year === 2026 ? "3/21/2026 8:00 AM" :
+	  year === 2026 ? "3/22/2025 8:00 AM" :
+	  year === 2024 ? "3/23/2024 8:00 AM" :
 	  year === 2023 ? "3/25/2023 8:00 AM" :
 	  "3/23/2024 8:00 AM"
 	);
