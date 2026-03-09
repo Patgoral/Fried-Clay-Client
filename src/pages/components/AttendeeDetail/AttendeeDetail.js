63c357c50fc0c0dc1195cd5c6357c7200ac657cd
patGoral <patgoral@gmail.com>
2026-03-09 15:59:12 -0400

    links

--- src/pages/components/AttendeeDetail/AttendeeDetail.js
+++ src/pages/components/AttendeeDetail/AttendeeDetail.js
@@ -8,7 +8,7 @@ import logo2023 from '../../../images/fc23logo.png'
 import logo2024 from '../../../images/fried-clay.png'
 import logo2025 from '../../../images/FriedClay5_v1-01.png'
 import logo2026 from '../../../images/FriedClay200k26.png'
-import SocialLinks from '../components/SocialLinks/SocialLinks'
+import SocialLinks from '../../components/SocialLinks/SocialLinks'
 
 import { Link } from 'react-router-dom'
 
