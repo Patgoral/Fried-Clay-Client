e618d87d33538b1c392aa3fd9b00dc317e9abe2f
patGoral <patgoral@gmail.com>
2026-03-29 09:59:33 -0400

    date change

--- src/pages/Results2026/Results2026.js
+++ src/pages/Results2026/Results2026.js
@@ -7,7 +7,7 @@ import logo from '../../images/FriedClay200k26.png'
 import SocialLinks from '../components/SocialLinks/SocialLinks'
 import Sponsors from '../components/Sponsors/Sponsors'
 
-const endDate = new Date('2026-03-30T00:00:00')
+const endDate = new Date('2026-03-29T00:00:00')
 const startDate = new Date('2026-03-21T08:00:00-04:00')
 
 export default function EventPage() {
